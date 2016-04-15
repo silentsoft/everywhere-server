4deb5bd7595457b1a901797b6630cccb2ce73205
silentsoft <hyesung.lee@silentsoft.org>
2016-04-16 02:07:42 +0900

    changed rest api path

--- src/main/java/org/silentsoft/everywhere/server/fx/store/controller/StoreController.java
+++ src/main/java/org/silentsoft/everywhere/server/fx/store/controller/StoreController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/fx/sm/store")
+@RequestMapping("/fx/store")
 @MultipartConfig
 public class StoreController {
 	@Autowired
